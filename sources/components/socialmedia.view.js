4d150309e9a8fe62f7c4b452829529453fae2a7f
Alejandro Torres <atorresalcala.me@gmail.com>
2021-03-31 14:38:05 +0200

    feat(behaviour): color mode

--- sources/components/socialmedia.view.js
+++ sources/components/socialmedia.view.js
@@ -1,11 +1,17 @@
 class SocialMediaView {
 	constructor() {
-		
+		this.GUI = {
+			toggle: document.getElementById('toggle'),
+		};
 	}
 
 	initListeners = () => {
-	
+		this.GUI.toggle.addEventListener('change', this._colorMode);
 	};
 
-
+	_colorMode = () => {
+		this.GUI.toggle.checked
+			? document.documentElement.setAttribute('data-theme', 'dark')
+			: document.documentElement.setAttribute('data-theme', 'light');
+	};
 }
